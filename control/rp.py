f23344b8669a9297c8a9cf173e68411097de0b4f
guidouzuomu <guidouzuomu@gmail.com>
2025-07-04 19:56:00 +0900

    a

--- control/rp.py
+++ control/rp.py
@@ -3,8 +3,10 @@ from MyHand import MyGripper_H100
 import time
 if __name__=="__main__":
     print("port")
+    print("Device says its ID is", hand.get_gripper_Id())
+
     hand=MyGripper_H100("/dev/ttyCH343USB0", 115200,id=14)
-    print("Device says its ID is", g.get_gripper_Id())
+    print("Device says its ID is", hand.get_gripper_Id())
     print(hand.port)
     print(hand.ser)
     hand.set_gripper_pose(0,0)
